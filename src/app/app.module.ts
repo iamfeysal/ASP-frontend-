9987578102f643d8e5e474d5eab8afdfaea08184
feysal <iamfeysal@gmail.com>
2019-09-01 00:03:54 +0300

    adding layout

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -12,6 +12,11 @@ import { HomeComponent } from './pages/home/home.component';
 import { LoginComponent } from './pages/login/login.component';
 import { SignupComponent } from './pages/signup/signup.component';
 import { ErrorComponent } from './ui/error/error.component';
+// tslint:disable-next-line:import-spacing
+import { MatToolbarModule, MatIconModule, MatSidenavModule, MatListModule, MatButtonModule } from  '@angular/material';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { HeaderComponent } from './shared/header/header.component';
+import { FooterComponent } from './shared/footer/footer.component';
 
 @NgModule({
   declarations: [
@@ -20,12 +25,20 @@ import { ErrorComponent } from './ui/error/error.component';
     LoginComponent,
     SignupComponent,
     ErrorComponent,
+    HeaderComponent,
+    FooterComponent,
   ],
   imports: [
     BrowserModule.withServerTransition({ appId: 'serverApp' }),
     AppRoutingModule,
     HttpClientModule,
     FormsModule,
+    BrowserAnimationsModule,
+    MatToolbarModule,
+    MatSidenavModule,
+    MatListModule,
+    MatButtonModule,
+    MatIconModule
   ],
   providers: [
     CookieService
